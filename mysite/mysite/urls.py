fb472b2871340c6d12a99e49b2aaf59c0b7365f2
Shubhit Chouhan <shubhitchouhan@gmail.com>
2018-12-04 18:14:57 +0530

    Updated Version

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),
 ]
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
